3858b02e44cfd0bb07acae8fa2118b0a03f74e2f
szolarisz <thomas@nyirszikszi.hu>
2021-10-04 21:16:35 +0200

    komment kiegészítve

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const fs = require('fs');
-const port = 4444;
+const port = 4444; //standard port
 
 const server = http.createServer(function (req, res) {
 
     // Router
-    switch(true) {
+    switch (true) {
         case req.url === '/' && req.method === 'GET':
             fs.readFile('./views/home.html', (err, file) => {
                 res.setHeader('content-type', 'text/html');
@@ -13,25 +13,26 @@ const server = http.createServer(function (req, res) {
             });
             break;
         case req.url === '/forma.css' && req.method === 'GET':
-                fs.readFile('./views/formas.css', (err, file) => {
-                    res.setHeader('content-type', 'text/css');
-                    res.end(file);
-                });
+            fs.readFile('./views/formas.css', (err, file) => {
+                res.setHeader('content-type', 'text/css');
+                res.end(file);
+            });
             break;
-            
+
         case req.url === '/phones' && req.method === 'GET':
             fs.readFile('./phones.json', (err, file) => {
                 res.setHeader('content-type', 'application/json');
                 res.end(file);
             });
             break;
-        default:   
-            res.end('404');    
+        default:
+            res.end('404');
     }
 
 
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+// 
 
 server.listen(port);
