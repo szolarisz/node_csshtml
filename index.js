9d5bff10926335fca0b4165091ecc771e478e18b
szolarisz <thomas@nyirszikszi.hu>
2021-10-04 21:07:40 +0200

    Server with css

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const port = 4444;
+
+const server = http.createServer(function (req, res) {
+
+    // Router
+    switch(true) {
+        case req.url === '/' && req.method === 'GET':
+            fs.readFile('./views/home.html', (err, file) => {
+                res.setHeader('content-type', 'text/html');
+                res.end(file);
+            });
+            break;
+        case req.url === '/forma.css' && req.method === 'GET':
+                fs.readFile('./views/formas.css', (err, file) => {
+                    res.setHeader('content-type', 'text/css');
+                    res.end(file);
+                });
+            break;
+            
+        case req.url === '/phones' && req.method === 'GET':
+            fs.readFile('./phones.json', (err, file) => {
+                res.setHeader('content-type', 'application/json');
+                res.end(file);
+            });
+            break;
+        default:   
+            res.end('404');    
+    }
+
+
+});
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+
+server.listen(port);
